c6aa0a69177f8b8a34dd081f1fea05ad605a2c58
Leonardo Dognani <82122343+Leonardodognani@users.noreply.github.com>
2021-07-09 19:58:55 -0300

    Will (#3)
    * Feat: layout navbar concluído
    
    * fix: Alteração do z-index da navbar e adicção do component header à home
    
    * feat: (header)Necessita ajustes
    
    * some changes in layout and naming conventions
    
    Co-authored-by: WdvOps <dvops@outlook.com>
    Co-authored-by: 4lexRossi <devalexrossi@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
