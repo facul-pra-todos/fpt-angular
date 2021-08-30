f1abde7be00d87e1cda52bc7536031fb74d467a5
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-08-29 21:46:27 -0300

    Created student Card and body wrapper

---
+++ src/app/shared/components/campaign-card/campaign-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CampaignCardComponent } from './campaign-card.component';
+
+describe('CampaignCardComponent', () => {
+  let component: CampaignCardComponent;
+  let fixture: ComponentFixture<CampaignCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CampaignCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CampaignCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
